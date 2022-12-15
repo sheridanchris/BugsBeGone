3d1a8fce3bb425202c3d7eb3898177f588a4aeda
Christian Sheridan <christiansheridan@outlook.com>
2022-12-15 12:24:00 -0600

    Create solution and add boilerplate project files

---
+++ src/Client/App.fs
@@ -0,0 +1,11 @@
+﻿module App
+
+open Lit
+open Fable.Core.JsInterop
+
+importSideEffects "./index.css"
+
+[<LitElement("my-app")>]
+let MyApp() =
+  let _ = LitElement.init (fun cfg -> cfg.useShadowDom <- false)
+  html $"<p>Hello, World!</p>"
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    commonjsOptions: {
+      include: ["/node_modules"]
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  }
+})
